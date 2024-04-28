81d9986070401e31aece3097700a2fb0d39e39ab
LiadJ9 <127025326+LiadJ9@users.noreply.github.com>
2024-04-28 18:42:19 +0300

    Github host

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { GenericContextProvider } from './contexts';
 import App from './App.jsx';
 import './index.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/Personal-website-2/',
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GenericContextProvider>
-      <App />
+      <RouterProvider router={router} />
     </GenericContextProvider>
   </React.StrictMode>
 );
